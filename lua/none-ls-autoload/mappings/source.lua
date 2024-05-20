947a1aed6fe06f4d8f4cf4e51502df7e6a402549
Zeioth <zeioth@hotmail.com>
2024-05-20 21:24:24 +0200

    reformat(conventions): `_` is now consistently renamed to `f` in our codebase.

--- lua/none-ls-autoload/mappings/source.lua
+++ lua/none-ls-autoload/mappings/source.lua
@@ -1,12 +1,12 @@
 -- Library to convert betwen mason and none-ls names.
 
 -- mason functional programming libraries.
-local _ = require('mason-core.functional')
+local f = require('mason-core.functional')
 local o = require('mason-core.optional')
 
 local M = {}
 
--- Maps mason package names to its none-ls name for the packages that need it.
+-- maps mason package names to its none-ls name for the packages that need it.
 local package_to_none_ls = {
 	['cmake_lint'] = 'cmakelint',
 	['cmake_format'] = 'cmakelang',
@@ -26,10 +26,10 @@ local package_to_none_ls = {
 
 ---Given a mason pkg name, use its none-ls source name equivalent.
 ---@return string name none-ls source name.
-M.get_none_ls_source_name = _.memoize(function(source)
+M.get_none_ls_source_name = f.memoize(function(source)
 	local name = o.of_nilable(
 		package_to_none_ls[source]):or_else_get(
-  		_.always(source:gsub('%_', '-')
+  		f.always(source:gsub('%_', '-')
   	)
 	)
 
